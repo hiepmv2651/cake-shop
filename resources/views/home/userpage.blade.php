5df9412d052b8ed83cf4cddda8ab30e7e6a6936e
Khanh Tran <khanh.tv.soen@gmail.com>
2022-11-02 20:09:57 +0700

    update homepage

---
+++ resources/views/home/userpage.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Basic -->
+    @include('home.css2')
+</head>
+
+<body>
+
+    <div class="hero_area">
+        <!-- header section strats -->
+        @include('home.header')
+        <!-- end header section -->
+        <!-- slider section -->
+        @include('home.slider')
+        <!-- end slider section -->
+    </div>
+    <!-- why section -->
+    @include('home.why')
+    <!-- end why section -->
+
+    <!-- arrival section -->
+    @include('home.new_arrival')
+    <!-- end arrival section -->
+
+    <!-- product section -->
+
+    <!-- end product section -->
+
+    <!-- subscribe section -->
+    @include('home.subscribe')
+    <!-- end subscribe section -->
+
+    <!-- client section -->
+    @include('home.client')
+    <!-- end client section -->
+
+    <!-- footer start -->
+    @include('home.footer')
+    <!-- footer end -->
+
+    <div class="cpy_">
+        <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
+
+            Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
+
+        </p>
+    </div>
+    <script>
+        document.addEventListener("DOMContentLoaded", function(event) {
+            var scrollpos = localStorage.getItem('scrollpos');
+            if (scrollpos) window.scrollTo(0, scrollpos);
+        });
+
+        window.onbeforeunload = function(e) {
+            localStorage.setItem('scrollpos', window.scrollY);
+        };
+    </script>
+    <!-- jQery -->
+    @include('home.js2')
+
+</body>
+
+</html>

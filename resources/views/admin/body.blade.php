0e5cde82fef2336d620b045ed3f976a4ce5bb313
unknown <khuevotan@gmail.com>
2022-11-02 20:34:41 +0700

    update admin page

---
+++ resources/views/admin/body.blade.php
@@ -0,0 +1,208 @@
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    {{-- <h3 class="mb-0">{{$total_products}}</h3> --}}
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Total Products</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    {{-- <h3 class="mb-0">{{$total_orders}}</h3> --}}
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success">
+                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Total Orders</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    {{-- <h3 class="mb-0">{{$total_users}}</h3> --}}
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-danger">
+                                    <span class="mdi mdi-arrow-bottom-left icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Total Customers</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    {{-- <h3 class="mb-0">{{$sum_orders}}</h3> --}}
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Sum price orders</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    {{-- <h3 class="mb-0">{{$total_devivered}}</h3> --}}
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Order Delivered</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-sm-6 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-9">
+                                <div class="d-flex align-items-center align-self-start">
+                                    {{-- <h3 class="mb-0">{{$total_processing}}</h3> --}}
+                                </div>
+                            </div>
+                            <div class="col-3">
+                                <div class="icon icon-box-success ">
+                                    <span class="mdi mdi-arrow-top-right icon-item"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <h6 class="text-muted font-weight-normal">Order Processing</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                <div class="card-body">
+                        <h4 class="card-title">Visitors by Countries</h4>
+                        <div class="row">
+                            <div class="col-md-5">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <i class="flag-icon flag-icon-us"></i>
+                                                </td>
+                                                <td>USA</td>
+                                                <td class="text-right"> 1500 </td>
+                                                <td class="text-right font-weight-medium"> 56.35% </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <i class="flag-icon flag-icon-de"></i>
+                                                </td>
+                                                <td>Germany</td>
+                                                <td class="text-right"> 800 </td>
+                                                <td class="text-right font-weight-medium"> 33.25% </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <i class="flag-icon flag-icon-au"></i>
+                                                </td>
+                                                <td>Australia</td>
+                                                <td class="text-right"> 760 </td>
+                                                <td class="text-right font-weight-medium"> 15.45% </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <i class="flag-icon flag-icon-gb"></i>
+                                                </td>
+                                                <td>United Kingdom</td>
+                                                <td class="text-right"> 450 </td>
+                                                <td class="text-right font-weight-medium"> 25.00% </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <i class="flag-icon flag-icon-ro"></i>
+                                                </td>
+                                                <td>Romania</td>
+                                                <td class="text-right"> 620 </td>
+                                                <td class="text-right font-weight-medium"> 10.25% </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <i class="flag-icon flag-icon-br"></i>
+                                                </td>
+                                                <td>Brasil</td>
+                                                <td class="text-right"> 230 </td>
+                                                <td class="text-right font-weight-medium"> 75.00% </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div id="audience-map" class="vector-map"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- content-wrapper ends -->
+    <!-- partial:partials/_footer.html -->
+    <footer class="footer">
+        <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright ©
+                bootstrapdash.com 2020</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center"> Free <a
+                    href="https://www.bootstrapdash.com/bootstrap-admin-template/" target="_blank">Bootstrap
+                    admin templates</a> from Bootstrapdash.com</span>
+        </div>
+    </footer>
+    <!-- partial -->
+</div>
+<!-- main-panel ends -->
+</div>
+<!-- page-body-wrapper ends -->
+</div>
---
+++ resources/views/admin/js.blade.php
@@ -0,0 +1,37 @@
+<script src="admin/assets/vendors/js/vendor.bundle.base.js"></script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.12.1/js/dataTables.bulma.min.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable({
+            scrollX: true,
+            autoWidth: false,
+            columnDefs: [
+                {
+                    targets: ['_all'],
+                    className: 'mdc-data-table__cell',
+                },
+            ],
+        });
+    });
+</script>
+<script src="//unpkg.com/alpinejs" defer></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="admin/assets/vendors/chart.js/Chart.min.js"></script>
+<script src="admin/assets/vendors/progressbar.js/progressbar.min.js"></script>
+<script src="admin/assets/vendors/jvectormap/jquery-jvectormap.min.js"></script>
+<script src="admin/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+<script src="admin/assets/vendors/owl-carousel-2/owl.carousel.min.js"></script>
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="admin/assets/js/off-canvas.js"></script>
+<script src="admin/assets/js/hoverable-collapse.js"></script>
+<script src="admin/assets/js/misc.js"></script>
+<script src="admin/assets/js/settings.js"></script>
+<script src="admin/assets/js/todolist.js"></script>
+<!-- endinject -->
+<!-- Custom js for this page -->
+<script src="admin/assets/js/dashboard.js"></script>

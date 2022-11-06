4cc8d9413252dc33e745fb4b1b6d10d6da7c0700
Khanh Tran <khanh.tv.soen@gmail.com>
2022-11-06 15:30:53 +0700

    update model

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Cart extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsTo(Product::class, 'Product_id', 'id');
+    }
+    protected $fillable = ['price', 'quantity', 'Product_id', 'user_id', 'image'];
 }

001ae7cf284394682a7c813265f367ea963233d0
hiepmv2651 <vanhiep429496@gmail.com>
2022-11-06 14:13:36 +0700

    create crud table

---
+++ app/Http/Controllers/ChiTietHDController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChiTietHD;
+use App\Http\Requests\StoreChiTietHDRequest;
+use App\Http\Requests\UpdateChiTietHDRequest;
+
+class ChiTietHDController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreChiTietHDRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreChiTietHDRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ChiTietHD  $chiTietHD
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ChiTietHD $chiTietHD)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ChiTietHD  $chiTietHD
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChiTietHD $chiTietHD)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateChiTietHDRequest  $request
+     * @param  \App\Models\ChiTietHD  $chiTietHD
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateChiTietHDRequest $request, ChiTietHD $chiTietHD)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ChiTietHD  $chiTietHD
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ChiTietHD $chiTietHD)
+    {
+        //
+    }
+}
